720d7094d45173db2da7b7372a7c66c55e0074be
AetelFinch <maxchup30@gmail.com>
2021-09-26 20:01:14 +0700

    fix problems with git

--- 1_task/getopt_ex.c
+++ 1_task/getopt_ex.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
+<<<<<<< Updated upstream
 #include <ulimit.h>
 #include <sys/types.h>
 
+=======
+#include <sys/types.h>
+>>>>>>> Stashed changes
 
 int main(int argc, char *argv[])
 {
